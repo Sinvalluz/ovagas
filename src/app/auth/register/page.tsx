5b8854bf8f350236a3822b1a6a1dcb312979f55a
Sinval Luz <contato.sinvalluz@gmail.com>
2026-09-18 12:05:00 -0300

    feat: add logo SVG and integrate it into the registration page

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 import RegisterForm from "@/features/auth/register/components/register-form";
 
@@ -6,6 +7,14 @@ export default function Register() {
 		<div className="flex h-full justify-center items-center p-2">
 			<Card className="w-full max-w-md">
 				<CardHeader>
+					<Image
+						src={"/logo.svg"}
+						alt="Logo"
+						width={160}
+						height={100}
+						loading="eager"
+						className="text-foreground mb-8"
+					/>
 					<CardTitle className="text-4xl font-bold">Crie sua conta!</CardTitle>
 					<CardDescription className="text-lg">
 						Crie sua conta e gerencie suas vagas de emprego de forma simples e eficiente.
